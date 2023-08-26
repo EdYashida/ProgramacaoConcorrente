252559ad983983ac3d36858c0c8984d07aea2dbc
EdYashida <107697323+EdYashida@users.noreply.github.com>
2023-08-26 19:24:09 -0300

    Create Loteria.java

---
+++ Loteria.java
@@ -0,0 +1,24 @@
+
+package loteria;
+
+
+public class Loteria {
+
+
+    public static void main(String[] args) {
+
+        //ler arquivo
+        //dividir arquivo em linhas
+        //count para cada sequencia
+        //ler linhas de forma paralela
+        //incrementar count quando cada sequencia aparecer em uma linha
+        //retornar counts
+        
+        LeiaCVS obj = new LeiaCVS();
+        obj.run();
+
+        
+        
+    }
+    
+}
