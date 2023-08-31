b889a96023923c347652e26257a28b1948ed9c08
EdYashida <107697323+EdYashida@users.noreply.github.com>
2023-08-31 10:32:18 -0300

    Update Loteria.java

--- Loteria.java
+++ Loteria.java
@@ -1,24 +1,27 @@
 
 package loteria;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 
 public class Loteria {
 
-
-    public static void main(String[] args) {
-
-        //ler arquivo
-        //dividir arquivo em linhas
-        //count para cada sequencia
-        //ler linhas de forma paralela
-        //incrementar count quando cada sequencia aparecer em uma linha
-        //retornar counts
+    private static final ExecutorService pool = Executors.newFixedThreadPool(2);
+    
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+          
+        LeiaCVS obj1 = new LeiaCVS(2,1400);
+        LeiaCVS obj2 = new LeiaCVS(1401,2892);
         
-        LeiaCVS obj = new LeiaCVS();
-        obj.run();
+        Future<int[]> futuro1 = pool.submit(obj1);
+        Future<int[]> futuro2 = pool.submit(obj2);
+
 
         
-        
+        pool.shutdown();
     }
     
 }
